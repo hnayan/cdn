8fe6c6e565cdda5aa7d3a79994d06669d72c52a1
hnayan <hnayan@chtsinc.com>
2021-06-27 13:58:37 +0530

    first commit

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -12,35 +12,35 @@ import com.cdn.utils.BaseClass;
 import com.cdn.utils.TestData;
 
 public class LoginTest extends BaseClass {
-	
+
 	LoginPage loginPage;
 	HomePage homepage;
 	WelcomePage welcomepage;
-  
-  @BeforeMethod
-  public void beforeMethod(Method m) {
-	  loginPage = new LoginPage();
-	  welcomepage = new WelcomePage();
-	  homepage = new HomePage();
-	  
-  }
-  
 
-  @Test  
-  public void verifyLoginPage() {
-	  
-	  welcomepage.clickClose();
-	  homepage.clickMoreOptions();
-	  homepage.clickAccountButton();
-	  homepage.clickExistingSubscribeLink();
-	  loginPage.enterUserName(TestData.USERNAME);
-	  loginPage.enterPassword(TestData.PASSWORD);
-	  loginPage.clickLogin();
-	  String expectedTitle = "EXISTING SUBSCRIPTIONS";
-	  Assert.assertEquals(loginPage.loginPageheader(), expectedTitle);
-	  
-  }
-  
-  
+	@BeforeMethod
+	public void beforeMethod(Method m) {
+		loginPage = new LoginPage();
+		welcomepage = new WelcomePage();
+		homepage = new HomePage();
+
+	}
+
+
+	@Test  
+	public void verifyLoginPage() {
+
+		welcomepage.clickClose();
+		homepage.clickMoreOptions();
+		homepage.clickAccountButton();
+		homepage.clickExistingSubscribeLink();
+		loginPage.enterUserName(TestData.USERNAME);
+		loginPage.enterPassword(TestData.PASSWORD);
+		loginPage.clickLogin();
+		String expectedTitle = "EXISTING SUBSCRIPTIONS";
+		Assert.assertEquals(loginPage.loginPageheader(), expectedTitle);
+
+	}
+
+
 
 }
